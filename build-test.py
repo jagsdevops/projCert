52cbfd25fb8ffb7ab4397b48c1fdd63bbc2485ad
Jags Mundey <vagrant@node01.example.com>
2018-10-19 16:55:38 +0000

    changing wait time for site to open in build-test

--- build-test.py
+++ build-test.py
@@ -7,7 +7,7 @@ options.add_argument("--headless");
 options.binary='/usr/bin/chromium-browser'
 #driver_path='/home/edureka/bin/chromedriver'
 browser = webdriver.Chrome(chrome_options=options) #,executable_path=driver_path); commenting as will try to install desired chromedriver in default path and run the case
-time.sleep(20)
+time.sleep(120)
 browser.get("http://127.0.0.1:8080/");
 time.sleep(10)
 link = browser.find_element_by_link_text('About Us')
